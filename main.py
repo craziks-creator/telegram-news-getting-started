0d103adee7b196d21e3f665266348aa7f5839676
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-01-29 19:45:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from telegram_news.template import InfoExtractor, NewsPostman, InfoExtractorJSON, NewsPostmanJSON
-from telegram_news.utils import xml_to_json
-bot_token = os.getenv("TOKEN")
+from telegram_news.utils import xml_to_jsont_token = os.getenv("TOKEN")
 channel = os.getenv("CHANNEL")
 channel2 = os.getenv("CHANNEL2")
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -20,10 +19,10 @@ def ssc_id_policy(link):
 #-------------------------channel 2----------------------------------#
 
 url2 = "https://sscnr.nic.in/newlook/site/Whatsnew.html"
-url7 = "https://sscnr.nic.in/newlook/site/application_status.html"
-url6 = "https://sscnr.nic.in/newlook/site/admit_card.html"
-tag2 = "Nrnews2"
-table_name2 = "Nrnews2"
+#url7 = "https://sscnr.nic.in/newlook/site/application_status.html"
+#url6 = "https://sscnr.nic.in/newlook/site/admit_card.html"
+tag2 = "Nrnews"
+table_name2 = "Nrnews"
 
 # Info extractor to process data format
 ie1 = InfoExtractor()
@@ -37,7 +36,7 @@ ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
 
 # News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url2,url7,url6, ], sendList=[channel,channel2, ], db=db, tag=tag2)
+np1 = NewsPostman(listURLs=[url2, ], sendList=[channel,channel2, ], db=db, tag=tag2)
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name2)
@@ -102,36 +101,35 @@ np1.poll()
 """
 #-------------------------channel 5----------------------------------#
 
-url5 = "https://sssc.uk.gov.in/"
-tag5 = "uksssc"
-table_name5 = "uksssc"
+url5 = "https://sscnr.nic.in/newlook/site/admit_card.html"
+tag5 = "admitcard"
+table_name5 = "admitcard"
 
 # Info extractor to process data format
 ie1 = InfoExtractor()
 
 # Select elements by CSS-based selector
-ie1.set_list_selector('body > #container') #id_ul_li
-ie1.set_title_selector('#midColumn > p')  #id
-ie1.set_paragraph_selector('strong')
-ie1.set_time_selector(' ')
+ie1.set_list_selector('div.inner_page > ul ') #id_ul_li
+ie1.set_title_selector('div.inner_page > ul > li ')  #id
+ie1.set_paragraph_selector('div.inner_page > ul > li ')
+ie1.set_time_selector('')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
-ie1.set_id_policy(ssc_id_policy)
 
 # News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url5, ], sendList=[channel2, ], db=db, tag=tag5)
+np1 = NewsPostman(listURLs=[url5, ], sendList=[channel2,channel2, ], db=db, tag=tag5)
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name5)
 np1.set_max_list_length(45)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
-
+"""
 #-------------------------channel 6----------------------------------#
 
 url6 = "https://sscnr.nic.in/newlook/site/admit_card.html"
-tag6 = "sscadmit2"
-table_name6 = "admit2"
+tag6 = "sscadmit"
+table_name6 = "admit"
 
 # Info extractor to process data format
 ie1 = InfoExtractor()
@@ -146,7 +144,7 @@ ie1.max_post_length = 2000
 ie1.set_id_policy(ssc_id_policy)
 
 # News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url6, ], sendList=[channel2, ], db=db, tag=tag6)
+np1 = NewsPostman(listURLs=[url6, ], sendList=[channel2,channel2, ], db=db, tag=tag6)
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name6)
@@ -157,8 +155,8 @@ np1.poll()
 #-------------------------channel 7----------------------------------#
 
 url7 = "https://sscnr.nic.in/newlook/site/application_status.html"
-tag7 = "sscnrstatus2"
-table_name7 = "sscnrstatus2"
+tag7 = "sscnrstatus"
+table_name7 = "sscnrstatus"
 
 # Info extractor to process data format
 ie1 = InfoExtractor()
@@ -172,14 +170,14 @@ ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
 ie1.set_id_policy(ssc_id_policy)
 # News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url7, ], sendList=[channel2, ], db=db, tag=tag7)
+np1 = NewsPostman(listURLs=[url7, ], sendList=[channel2,channel2, ], db=db, tag=tag7)
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name7)
 np1.set_max_list_length(45)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
-"""
+
 #-------------------------channel 1----------------------------------#
 
 url1 = "https://ssc.nic.in/Portal/LatestNews"
