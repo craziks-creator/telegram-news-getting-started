f8cdf5149d0ed5c376fffbd196c86065881094fa
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-02-02 16:10:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,61 +71,6 @@ np1.set_table_name(table_name3)
 np1.set_max_list_length(15)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
-"""
-#-------------------------channel 4----------------------------------#
-
-url4 = "https://ssc.nic.in/MarksStatus/MarksStatusIndex"
-tag4 = "sscscore"
-table_name4 = "scorecard"
-
-# Info extractor to process data format
-ie1 = InfoExtractor()
-
-# Select elements by CSS-based selector
-ie1.set_list_selector('select#ddlExam > option') #id_ul_li
-ie1.set_title_selector('h1')  #id
-ie1.set_paragraph_selector('select#ddlExam > option')
-ie1.set_time_selector('span')
-#ie1.set_id_policy(ssc_id_policy)
-ie1.set_source_selector('span.sourceTemplate')
-ie1.max_post_length = 2000
-
-# News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url4, ], sendList=[channel,channel2 ], db=db, tag=tag4)
-np1.set_bot_token(bot_token)
-np1.set_extractor(ie1)
-np1.set_table_name(table_name4)
-ie1.set_id_policy(ssc_id_policy)
-np1.set_max_list_length(10)
-np1.set_max_table_rows(25 * 3, False)
-np1.poll()
-
-#-------------------------channel 5----------------------------------#
-
-url5 = "https://ssc.nic.in/Portal/Results"
-tag5 = "cporesult"
-table_name5 = "cpo"
-
-# Info extractor to process data format
-ie1 = InfoExtractor()
-
-# Select elements by CSS-based selector
-ie1.set_list_selector('div#noticeschsl.tab-content>tbody') #id_ul_li
-ie1.set_title_selector('div#noticeschsl.tab-content>tbody>tr>td:nth-child(4)>a')  #id
-ie1.set_paragraph_selector('div#noticeschsl.tab-content>tbody>tr>td:nth-child(4)>a')
-ie1.set_time_selector('span')
-ie1.set_source_selector('span.sourceTemplate')
-ie1.max_post_length = 2000
-
-# News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url5, ], sendList=[channel,channel2, ], db=db, tag=tag5)
-np1.set_bot_token(bot_token)
-np1.set_extractor(ie1)
-np1.set_table_name(table_name5)
-np1.set_max_list_length(10)
-np1.set_max_table_rows(25 * 3, False)
-np1.poll()
-"""
 #-------------------------channel 6----------------------------------#
 
 url6 = "https://sscnr.nic.in/newlook/site/admit_card.html"
@@ -201,10 +146,9 @@ np1 = NewsPostman(listURLs=[url1, ], sendList=[channel,channel2, ], db=db, tag=t
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name1)
-np1.set_max_list_length(15)
+np1.set_max_list_length(20)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
-
 #-------------------------channel 2----------------------------------#
 """
 url2 = "https://createfeed.fivefilters.org/extract.php?url=https%3A%2F%2Fssc.nic.in%2FPortal%2FLatestNews&max=5&order=document&guid=0"
