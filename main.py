5b8ba6b0200046c6afc16c90564a8127a1e8fbbb
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-02-02 15:51:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 db = Session(bind=engine.connect())
 
-def ssc_id_policy(link):
-        return hashlib.md5(link.encode("utf-8")).hexdigest()
+#def ssc_id_policy(link):
+        #return hashlib.md5(link.encode("utf-8")).hexdigest()
 
 #-------------------------channel 2----------------------------------#
 
@@ -67,7 +67,7 @@ np1 = NewsPostman(listURLs=[url3, ], sendList=[channel,channel2, ], db=db, tag=t
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name3)
-ie1.set_id_policy(ssc_id_policy)
+#ie1.set_id_policy(ssc_id_policy)
 np1.set_max_list_length(15)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
@@ -86,7 +86,7 @@ ie1.set_list_selector('select#ddlExam > option') #id_ul_li
 ie1.set_title_selector('h1')  #id
 ie1.set_paragraph_selector('select#ddlExam > option')
 ie1.set_time_selector('span')
-ie1.set_id_policy(ssc_id_policy)
+#ie1.set_id_policy(ssc_id_policy)
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
 
@@ -142,7 +142,7 @@ ie1.set_paragraph_selector('div.inner_page > ul > li ')
 ie1.set_time_selector('')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
-ie1.set_id_policy(ssc_id_policy)
+#ie1.set_id_policy(ssc_id_policy)
 
 # News postman to manage sending affair
 np1 = NewsPostman(listURLs=[url6, ], sendList=[channel,channel2, ], db=db, tag=tag6)
@@ -169,7 +169,7 @@ ie1.set_paragraph_selector('div.inner_page > ul > li ')
 ie1.set_time_selector('')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
-ie1.set_id_policy(ssc_id_policy)
+#ie1.set_id_policy(ssc_id_policy)
 
 # News postman to manage sending affair
 np1 = NewsPostman(listURLs=[url7, ], sendList=[channel,channel2, ], db=db, tag=tag7)
